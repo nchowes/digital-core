864750b508f0d396f8e5312a5497ed46b85e0099
nchowes <nchowes@gmail.com>
2022-03-07 19:04:52 -0500

    initial geochem refactor

--- minalyze/__init__.py
+++ minalyze/__init__.py
@@ -1,15 +1,16 @@
 """
-Machine learning module for Minalyze
-====================================
+    minalyze
+    ----------------------------------------
+    
+    Machine learning for digital core. 
 
-Add description
-
-See XXXX for complete documentation
+    TODO Add description
+    See XXXX for complete documentation
 """
 
 __version__ = '0.0.1'
 
-from .core import Geochem, GeochemML
+from .geochemistry import Geochem, GeochemML
 
-__all__ = [ 'geochem', 'Geochem', 'GeochemML' ]
+__all__ = ['Geochem', 'GeochemML']
 
